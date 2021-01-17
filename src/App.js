f180a26936364086cbd4a711ace255e9dfbdf7a1
himasheth <himasheth05@gmail.com>
2021-01-17 18:47:41 -0500

    top

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="top">
+            <table>
+        <div className="title">
+          Hima Sheth’s Design <br></br>Portfolio
+        </div>
+        <div className="subtitle">✨ I’m a dev and aspiring designer ✨</div>
+        <div className="intro">
+          I’m passionate about <b>tech for social good </b>and hope to <br></br>
+          develop and design impactful, and easy-to-use <br></br>solutions to
+          the world’s problems!
+        </div>
+        </table>
+        <div id="circle"></div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
