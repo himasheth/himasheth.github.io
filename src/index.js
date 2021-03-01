ac9982f3663771cff1cc8728ec5aa630e95533dd
himasheth <himasheth05@gmail.com>
2021-02-28 23:00:44 -0500

    print statement

--- src/index.js
+++ src/index.js
@@ -10,3 +10,4 @@ import { BrowserRouter as Router} from 'react-router-dom';
 
 
 ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById('root'));
+console.log(process.env.PUBLIC_URL);
