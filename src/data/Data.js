14f6574f9e58626c195f8f815ee4ff29dcd7b79d
himasheth <himasheth05@gmail.com>
2021-06-14 12:01:09 -0400

    line breaks

--- src/data/Data.js
+++ src/data/Data.js
@@ -12,7 +12,8 @@ export const projectList = [
     {
       'name': 'PosiTweet',
       'description': 'PosiTweet: A web app designed to show you positive vibes only!',
-      'role': 'I helped design and implement the entire UI. The design was done in Figma after thorough research on the sentiment we were trying to evoke when people used our site.',
+      'role': 'I helped design and implement the entire UI. The design was done in Figma after thorough research on the sentiment ' + 
+              'we were trying to evoke when people used our site.',
       'link': 'https://www.figma.com/file/xGBdLG2ZLWEjRIH7o78j5r/Hack-The-North!!?node-id=0%3A1',
       'image': positweet,
     },
@@ -35,7 +36,8 @@ export const projectList = [
   
     {
       'name': 'Shad Summer Program',
-      'description': 'Design thinking challenge where SHAD students spend their summer researching, protoyping, creating, and testing a product based on the prompt given. The prompt for my year was to impactfully reduce waste.',
+      'description': 'Design thinking challenge where SHAD students spend their summer researching, protoyping, creating, and testing a product' +
+                     ' based on the prompt given. The prompt for my year was to impactfully reduce waste.',
       'role': 'My team used spectroscopy and machine learning to create a scanner that tells you whether or not your waste is recyclable.',
       'link': 'https://docs.google.com/presentation/d/15dg5iaqr4HNzTBQKRRF1UxYeIyTEin1qV2OHlfWb1vA/edit?usp=sharing',
       'image': shad,
@@ -52,7 +54,8 @@ export const projectList = [
     {
       'name': 'DECA Business Plan Design',
       'description': 'Conducted a new marketing plan to attract more customers for a retail brand',
-      'role': 'During DECA, a high school business case competition, a peer and I designed a user-centered marketing plan. We conducted user research and proposed changes to the current marketing plan as well addition of new plans to attract a new user type.',
+      'role': 'During DECA, a high school business case competition, a peer and I designed a user-centered marketing plan. We conducted user research ' +
+              'and proposed changes to the current marketing plan as well addition of new plans to attract a new user type.',
       'link': 'https://docs.google.com/document/d/1IX5qaqPEnACtZgtsnKrc6NviJLRFTj-uGYUvv04Y1wg/edit?usp=sharing',
       'image': deca,
     },
@@ -61,22 +64,30 @@ export const projectList = [
   export const extracurricularList = [
     {
       'name': 'Dimagi | Technology for Social Impact',
-      'description': 'Dimagi works with countries all around the world to provide tech for social impact. Their tech development platform CommCare is used globally by a diverse group of people in order to create emerging technology guaranteed to change the way we run our health care systems.',
-      'role': "I'm working on a team of four to create a new CommCare app that will help developing countries combat the spread of tuberculosis. My specific role includes researching the user personas, designing the prototype of the app, and researching current design flaws to ensure our design is easily accessible by anyone regardless of their age, gender, and technical literacy.",
+      'description': 'Dimagi works with countries all around the world to provide tech for social impact. Their tech development platform CommCare is ' +
+                     'used globally by a diverse group of people in order to create emerging technology guaranteed to change the way we run our health care systems.',
+      'role': "I'm working on a team of four to create a new CommCare app that will help developing countries combat the spread of tuberculosis. My specific role " + 
+              "includes researching the user personas, designing the prototype of the app, and researching current design flaws to ensure our design is easily accessible " +
+              "by anyone regardless of their age, gender, and technical literacy.",
       'link': 'https://www.dimagi.com/',
       'image': dimagi,
     },
     {
       'name': 'UW Blueprint',
-      'description': 'UW Blueprint is a group of students at the University of Waterloo dedicated to building and promoting technology for social good. UW Blueprint partners with non-profits to provide technology services such as websites, mobile applications, analysis tools—free of charge.',
-      'role': "I'm a project developer on UW Blueprint's Small Projects team but I strive to involve myself in numerous other ways. I work with the designers to help with the iterative design process, giving feedback and testing workflows before I implement them using React, Typescript, and Sass. I'm also on the Meta-BP team that works to ensure UW Blueprint's future. Finally, I'm on the scoping committee and work to find new organizations that we can partner with.",
+      'description': 'UW Blueprint is a group of students at the University of Waterloo dedicated to building and promoting technology for social good. ' + 
+                     'UW Blueprint partners with non-profits to provide technology services such as websites, mobile applications, analysis tools—free of charge.',
+      'role': "I'm a project developer on UW Blueprint's Small Projects team but I strive to involve myself in numerous other ways. I work with the" + 
+              "designers to help with the iterative design process, giving feedback and testing workflows before I implement them using React, Typescript, and " +
+              "Sass. I'm also on the Meta-BP team that works to ensure UW Blueprint's future. Finally, I'm on the scoping committee and work to find new organizations that we can partner with.",
       'link': 'https://uwblueprint.org/',
       'image': blueprint,
     },
     {
       'name': 'UW Entrepreneurship Society',
-      'description': "EntSoc aims to build inclusive innovation by exposing students of all faculties to Waterloo's entrepreneurial ecosystem. We recently launched a project called 'Janus' that connects student entreprenuers at UW with other students who want to join them on their venture.",
-      'role': 'I work as a fullstack developer at UW Entrepreneurship Society. I use technologies like React, Typescript, Heroku, MongoDB, Sass, and Material-UI in order to implement new designs.',
+      'description': "EntSoc aims to build inclusive innovation by exposing students of all faculties to Waterloo's entrepreneurial ecosystem. We " + 
+                     "recently launched a project called 'Janus' that connects student entreprenuers at UW with other students who want to join them on their venture.",
+      'role': 'I work as a fullstack developer at UW Entrepreneurship Society. I use technologies like React, Typescript, Heroku, MongoDB, Sass, and ' + 
+              'Material-UI in order to implement new designs.',
       'link': 'https://entsoc.ca/janus',
       'image': entsoc,
     }
